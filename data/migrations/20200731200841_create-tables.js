382a9998dd4f549b1e703a7a41387adc05f81796
ryanp <ryanpanimation@gmail.com>
2020-07-31 23:05:10 -0500

    last updates to migrations and seeds

--- data/migrations/20200731200841_create-tables.js
+++ data/migrations/20200731200841_create-tables.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
       tbl.increments();
       tbl.string("project_name", 128).notNullable().unique();
       tbl.string("project_description");
-      tbl.boolean("project_complete").defaultsTo(0);
+      tbl.boolean("project_complete").notNullable().defaultsTo(false);
     })
     .createTable("resources", (tbl) => {
       tbl.increments();
@@ -17,7 +17,7 @@ exports.up = function (knex) {
       tbl.increments();
       tbl.string("task_description").notNullable();
       tbl.string("notes");
-      tbl.boolean("task_complete").defaultsTo(0);
+      tbl.boolean("task_complete").notNullable().defaultsTo(false);
       tbl
         .integer("project_id")
         .notNullable()
