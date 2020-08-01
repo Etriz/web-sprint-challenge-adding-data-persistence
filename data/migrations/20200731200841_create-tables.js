2a5488006c2c9b32dc3fa0a5b08a6baa7b4810bc
ryanp <ryanpanimation@gmail.com>
2020-07-31 20:51:17 -0500

    make create-table migration

---
+++ data/migrations/20200731200841_create-tables.js
@@ -0,0 +1,54 @@
+const { table } = require("../db-config");
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments();
+      tbl.string("project_name", 128).notNullable().unique();
+      tbl.string("project_description");
+      tbl.boolean("project_complete").defaultsTo(0);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments();
+      tbl.string("resource_name", 128).notNullable().unique();
+      tbl.string("resource_description");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments();
+      tbl.string("task_description").notNullable();
+      tbl.string("notes");
+      tbl.boolean("task_complete").defaultsTo(0);
+      tbl
+        .integer("project_id")
+        .notNullable()
+        .unsigned()
+        .references("projects.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("projects-resources", (tbl) => {
+      tbl
+        .integer("project_id")
+        .notNullable()
+        .unsigned()
+        .references("projects.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("resource_id")
+        .notNullable()
+        .unsigned()
+        .references("resources.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.primary(["project_id", "resource_id"]);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("projects-resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
