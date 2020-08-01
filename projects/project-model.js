7f5cc43e269bddad836237474459e65cebc62749
ryanp <ryanpanimation@gmail.com>
2020-07-31 22:56:52 -0500

    add endpoints

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,7 +1,58 @@
 const db = require("../data/db-config.js");
 
-function find() {
+function findProjects() {
   return db("projects");
 }
 
-module.exports = { find };
+function findProjectById(id) {
+  return db("projects").where({ id }).first();
+}
+
+function findResources() {
+  return db("resources");
+}
+
+function findTasks() {
+  return db("tasks as t")
+    .join("projects as p", "p.id", "t.project_id")
+    .select(
+      "t.id",
+      "t.task_description",
+      "t.notes",
+      "t.task_complete",
+      "p.project_name",
+      "p.project_description"
+    );
+}
+
+function addProject(data) {
+  return db("projects").insert(data);
+}
+
+function addResource(data) {
+  return db("resources").insert(data);
+}
+
+function addTask(data) {
+  return db("tasks").insert(data);
+}
+
+//! STRETCH
+function update() {
+  return db("projects");
+}
+
+function remove(database, id) {
+  return db(`${database}`).where({ id }).del();
+}
+
+module.exports = {
+  findProjects,
+  findProjectById,
+  findResources,
+  findTasks,
+  addProject,
+  addResource,
+  addTask,
+  remove,
+};
--- projects/project-router.js
+++ projects/project-router.js
@@ -5,12 +5,87 @@ const Projects = require("./project-model.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  Projects.find()
+  Projects.findProjects()
     .then((projects) => {
       res.json(projects);
     })
     .catch((err) => {
       res.status(500).json({ message: "Failed to get projects" });
+    });
+});
+
+router.get("/resources", (req, res) => {
+  Projects.findResources()
+    .then((projects) => {
+      res.json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get projects" });
+    });
+});
+
+router.get("/tasks", (req, res) => {
+  Projects.findTasks()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get projects" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Projects.findProjectById(id)
+    .then((projects) => {
+      res.json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Failed to get project ${id}` });
+    });
+});
+
+router.post("/", (req, res) => {
+  Projects.addProject(req.body)
+    .then((data) => {
+      res.status(201).json({ id: data[0], ...req.body });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to add resource" });
+    });
+});
+
+router.post("/resources", (req, res) => {
+  // console.log(req.body);
+  Projects.addResource(req.body)
+    .then((data) => {
+      res.status(201).json({ id: data[0], ...req.body });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to add resource" });
+    });
+});
+
+router.post("/tasks", (req, res) => {
+  // console.log(req.body);
+  Projects.addTask(req.body)
+    .then((data) => {
+      res.status(201).json({ id: data[0], ...req.body });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to add task" });
+    });
+});
+
+router.delete("/:database/:id", (req, res) => {
+  // console.log(req.body);
+  const { database, id } = req.params;
+  Projects.remove(database, id)
+    .then((data) => {
+      res.status(200).json({ deleted: `${database} id ${id} deleted` });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to delete" });
     });
 });
 
